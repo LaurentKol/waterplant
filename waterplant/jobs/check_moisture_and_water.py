70f2010a7d248c2a71903b6118cbcc91cb33c5bd
Laurent Kol <laurent.kol@gmail.com>
2023-04-23 23:43:43 +0900

    Check moisture immediately after watering

--- waterplant/jobs/check_moisture_and_water.py
+++ waterplant/jobs/check_moisture_and_water.py
@@ -1,9 +1,12 @@
 import logging
 from typing import List
+from datetime import datetime, timedelta
 
 from waterplant.pot import Pot
+from waterplant.config import config
 from waterplant.homeassistant import hahelper
 from waterplant.jobs.water import water
+from waterplant.jobs.check_sensors import check_sensors
 
 def check_moisture_and_water(pots: List[Pot], scheduler):
     for pot in pots:
@@ -17,6 +20,7 @@ def check_moisture_and_water(pots: List[Pot], scheduler):
             if moisture_levels['average'] < pot.dryness_threshold:
                 logging.info(f'Watering {pot.name} ({moisture_levels["average"]}% moist =< {pot.dryness_threshold}% moist threshold)')
                 scheduler.add_job(water, kwargs={'pot': pot}, id=f'watering-{pot.name}', misfire_grace_time=600, coalesce=True, executor='watering')
+                scheduler.add_job(check_sensors, kwargs={'pots': [pot], 'sensor_types': ['moisture']}, id=f'check_sensors-{pot.name}', misfire_grace_time=600, run_date=datetime.now() + timedelta(seconds = config.watering_duration_seconds + 10), coalesce=True, executor='bluetooth')
             else:
                 logging.info(f'{pot.name} is not dry enough ({moisture_levels["average"]}% moist > {pot.dryness_threshold}% moist threshold)')
         else:
