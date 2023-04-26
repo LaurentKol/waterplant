3773ce2b06c99c2cb7dd1a0997ba187f0f8dce39
Laurent Kol <laurent.kol@gmail.com>
2023-04-26 21:29:46 +0900

    Set config default for check_sensors_freq_minutes

--- waterplant/config.py
+++ waterplant/config.py
@@ -6,7 +6,7 @@ source = confuse.YamlSource('config.yaml')
 unvalidated_config = confuse.RootView([source])
 template = {
     'api_listening_ip': str,
-    'check_sensors_freq_minutes': 30,
+    'check_sensors_freq_minutes': confuse.Integer(default=30),
     'sensor_types': confuse.StrSeq(default=['moisture']), #TODO: limit to (moisture|temperature|light|conductivity|battery)
     'check_moisture_and_water_freq_cron': { # Make sure this is less frequent then miflora_cache_timeout
         'day': confuse.Optional(confuse.String(default='*')),
